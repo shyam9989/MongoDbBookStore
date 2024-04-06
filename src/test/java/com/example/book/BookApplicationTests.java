b4fb23af8c0ee041b3988d0cc0c64bbf9c804fa5
shyam <shyam.9989@gmail.com>
2024-04-06 16:59:57 +0530

    removed test cases

--- src/test/java/com/example/book/BookApplicationTests.java
+++ src/test/java/com/example/book/BookApplicationTests.java
@@ -1,25 +1,12 @@
 package com.example.book;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.longThat;
 
-import java.util.logging.Logger;
-
-import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.logging.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
 class BookApplicationTests {
 
-	Logger logger=(Logger) LoggerFactory.getLogger(BookApplicationTests.class);
-
-	@Test
-	public void contextLoads(){
-
-		logger.info("Test case executing..");
-		assertEquals(true, true);
-	}
+	
 }
