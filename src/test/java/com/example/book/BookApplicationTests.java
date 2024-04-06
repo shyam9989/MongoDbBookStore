a8ef26cca72f3708db0c6238a9ca32e51381a66c
shyam <shyam.9989@gmail.com>
2024-04-06 16:56:49 +0530

    test cases added

--- src/test/java/com/example/book/BookApplicationTests.java
+++ src/test/java/com/example/book/BookApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.book;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.longThat;
+
+import java.util.logging.Logger;
+
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class BookApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	Logger logger=(Logger) LoggerFactory.getLogger(BookApplicationTests.class);
 
+	@Test
+	public void contextLoads(){
+
+		logger.info("Test case executing..");
+		assertEquals(true, true);
+	}
 }
